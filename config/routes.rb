2be428b4b33e78a9bc6e0d2f3d2a58eeeb4812ea
Okeke Arinze <trent9002@gmail.com>
2017-05-14 00:51:42 +0100

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope :api, defaults: { format: :json } , constraints: { subdomain: 'api' }, path: '/'  do
+    scope :v1, module: :v1 do
+  		post 'token' => 'user_token#create'
+		resource :user, only: [:create, :show, :update]
+	end
+  end 
 end
