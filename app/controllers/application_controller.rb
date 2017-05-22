7312b74c8087abc339276a3d2c30aa188f4daae4
Okeke Arinze <trent9002@gmail.com>
2017-05-22 08:01:33 +0100

    add camelization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
 	include Knock::Authenticable
-
+	#before_action :underscore_params!
 	
 	def get_current_user
 		@current_user = current_user
 	end
+
+	# private
+	# def underscore_params!
+ #    params.deep_transform_keys!(&:underscore)
+ #    end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
   belongs_to :user
   acts_as_votable
   acts_as_taggable
   validates_presence_of :title, :url
-  extend FriendlyId
-  friendly_id :name, use: :slugged
 end
