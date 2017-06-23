351f2c37081e4ca1647a1f79ffcdd1038f3b9eca
Okeke Arinze <trent9002@gmail.com>
2017-06-23 05:01:57 +0100

    increase throttle allowance. its fucking up my react app

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module PicTerest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use Rack::Throttle::Interval, :min => 2.0
+    config.middleware.use Rack::Throttle::Minute, :max => 60
   end
 end
