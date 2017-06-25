91c7eda65d69f2fa989b94df1e1c4dc9b837baa7
Okeke Arinze <trent9002@gmail.com>
2017-06-25 07:27:44 +0100

    soft auth some actions in some controller

--- app/controllers/v1/follows_controller.rb
+++ app/controllers/v1/follows_controller.rb
@@ -1,5 +1,6 @@
 class V1::FollowsController < ApplicationController
 	before_action :authenticate_user, except: [:show]
+	before_action :soft_authenticate_user, only: [:show]
 	before_action :get_user
 	before_action :get_current_user, except: [:show]
 
